22280bfbf02f93857ef70af19b27a6b179666c3a
jayal <jay.allen.strong@gmail.com>
2025-04-22 13:40:31 -0400

    is_higher function started

--- Day 8/Caesar Cipher 1/main.py
+++ Day 8/Caesar Cipher 1/main.py
@@ -4,8 +4,6 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
-
 
 def encrypt(original_text, shift_amount):
     cipher_text = ""
@@ -17,10 +15,3 @@ def encrypt(original_text, shift_amount):
 
 
 encrypt(original_text=text, shift_amount=shift)
-
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-
-# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
-
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
