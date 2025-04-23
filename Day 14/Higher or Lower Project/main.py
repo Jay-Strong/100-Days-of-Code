a89cfe29811a0a86caa6c19442af8493bd91a7bb
Jay Strong <jay.allen.strong@gmail.com>
2025-04-23 08:02:34 -0400

    refactor higher_lower_game function to move profile selection inside the game loop

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -13,40 +13,40 @@ def is_higher(u_selection: dict, c_selection: dict) -> bool | None:
 
 
 def higher_lower_game():
-    profile_1 = random.choice(data)
-    profile_2 = random.choice(data)
     computer_selection = 0
     user_selection = 0
     points = 0
+    proceed = True
+    while proceed:
+        profile_1 = random.choice(data)
+        profile_2 = random.choice(data)
+        print(logo)
+        print(f"\nCompare A: {profile_1["name"]}, a {profile_1["description"]}, from {profile_1["country"]}.")
+        print(vs)
+        print(f"\nAgainst B: {profile_2["name"]}, a {profile_2["description"]} from {profile_2["country"]}.")
 
-
-    print(logo)
-    print(f"\nCompare A: {profile_1["name"]}, a {profile_1["description"]}, from {profile_1["country"]}.")
-    print(vs)
-    print(f"\nAgainst B: {profile_2["name"]}, a {profile_2["description"]} from {profile_2["country"]}.")
-
-    user_selection = input("Who has more followers? Type 'A' or 'B': ").lower()
-
-    while user_selection != "a" and user_selection != "b":
-        print("Invalid input")
         user_selection = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-    if user_selection == "a":
-        user_selection = profile_1
-        computer_selection = profile_2
-    elif user_selection == "b":
-        user_selection = profile_2
-        computer_selection = profile_1
-    else:
-        print("Invalid input. Try again.")
-    
-    if is_higher(u_selection=user_selection, c_selection=computer_selection):
-        print("Correct!")
-        points += 1
-        print(f"Your current score is: {points}")
-    else:
-        print("Sorry. You lose.")
-        return
+        while user_selection != "a" and user_selection != "b":
+            print("Invalid input")
+            user_selection = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+        if user_selection == "a":
+            user_selection = profile_1
+            computer_selection = profile_2
+        elif user_selection == "b":
+            user_selection = profile_2
+            computer_selection = profile_1
+        else:
+            print("Invalid input. Try again.")
+        
+        if is_higher(u_selection=user_selection, c_selection=computer_selection):
+            print("Correct!")
+            points += 1
+            print(f"Your current score is: {points}")
+        else:
+            print("Sorry. You lose.")
+            proceed = False
 
 
 higher_lower_game()
