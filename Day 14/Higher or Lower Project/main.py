df9da9d5089ef0f0239346705158f6b6efc6befc
Jay Strong <jay.allen.strong@gmail.com>
2025-04-23 05:52:07 -0400

    is_higher function implemented

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -1,7 +1,7 @@
-from game_data import data
 import random
+from game_data import data
 from art import logo, vs
- # POOP!
+
 
 def is_higher(u_selection: dict, c_selection: dict) -> bool | None:
     if u_selection["follower_count"] > c_selection["follower_count"]:
@@ -13,9 +13,10 @@ def is_higher(u_selection: dict, c_selection: dict) -> bool | None:
 
 
 def higher_lower_game():
-    print(logo)
     profile_1 = random.choice(data)
     profile_2 = random.choice(data)
+
+    print(logo)
     print(f"\nCompare A: {profile_1["name"]}, a {profile_1["description"]}, from {profile_1["country"]}.")
     print(vs)
     print(f"\nAgainst B: {profile_2["name"]}, a {profile_2["description"]} from {profile_2["country"]}.")
@@ -28,10 +29,19 @@ def higher_lower_game():
 
     if user_selection == "a":
         user_selection = profile_1
+        computer_selection = profile_2
     elif user_selection == "b":
-        user_choice = profile_2
+        user_selection = profile_2
+        computer_selection = profile_1
     else:
         print("Invalid input. Try again.")
+    
+    if is_higher(u_selection=user_selection, c_selection=computer_selection):
+        print("Correct!")
+    else:
+        print("Sorry. Try again.")
 
 
 higher_lower_game()
+
+# py main.py
