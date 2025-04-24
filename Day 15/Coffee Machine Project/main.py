8e4ba5884d7244c9e5041a9b1d0ebd270bda7c6b
Jay Strong <jay.allen.strong@gmail.com>
2025-04-24 06:30:10 -0400

    refactor: add TODO comments for coffee machine functionality steps

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -1,2 +1,25 @@
 from coffee_data import MENU, resources as res
 from art import mug
+
+# TODO-1: Prompt user by asking “ What would you like? (espresso/latte/cappuccino): ”
+
+
+# TODO-2: Turn off the Coffee Machine by entering “ off ” to the prompt.
+
+
+# TODO-3: When the user enters “report” to the prompt, a report should be generated that shows
+   # the current resource values.
+
+
+# TODO-4: When the user chooses a drink, the program should check if there are enough
+    # resources to make that drink.
+
+
+# TODO-5: If there are sufficient resources to make the drink selected, then the program should
+    # prompt the user to insert coins.
+
+
+# TODO-6: Check that the user has inserted enough money to purchase the drink they selected.
+
+
+# TODO-7: Deduct the required ingredients from the coffee machine resources after a successful transaction.
