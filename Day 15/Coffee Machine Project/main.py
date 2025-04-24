ae3bceb4544c70a8f3adb7ab3a858944ae369cc8
Jay Strong <jay.allen.strong@gmail.com>
2025-04-24 16:27:11 -0400

    refactor: improve resource check messages and streamline prepare_drink function

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -13,46 +13,47 @@ def report() -> None:
             print(f"{key.capitalize()}: ${val:.2f}")
 
 
-def check_resources(beverage_type: str) -> str:
+def check_resources(beverage_type: str) -> str | None:
     beverage = MENU[beverage_type]
     if beverage["ingredients"]["water"] > res["water"]:
-        return f"Sorry, there is not enough water to make {beverage}."
+        return f"Sorry, there is not enough water to make {beverage_type}."
     elif beverage["ingredients"]["milk"] > res["milk"]:
-        return f"Sorry, there is not enough milk to make {beverage}."
+        return f"Sorry, there is not enough milk to make {beverage_type}."
     elif beverage["ingredients"]["coffee"] > res["coffee"]:
-        return f"Sorry, there is not enough coffee to make {beverage}."
+        return f"Sorry, there is not enough coffee to make {beverage_type}."
     else:
-        return f"Preparing your {beverage_type} now..."
+        return
     
-def prepare_drink(drink_name: str) -> None:
-    drink = MENU[drink_name]
-    check_resources(beverage_type=drink_name)
+
+
+
+def prepare_drink(selected_beverage: str) -> None:
+    beverage = MENU[selected_beverage]
+    check_resources(beverage_type=selected_beverage)
+    # TODO: Add logic for coin insertion and resource deduction
+    
+    print(f"Preparing your {selected_beverage}...")
+
+    
 
 def coffee_machine() -> None:
+    drink_options = {
+        "1": "espresso",
+        "2": "latte",
+        "3": "cappuccino"
+    }
     while True:
-        user_selection = input("What would you like? (1-espresso/2-latte/3-cappuccino):\n")
+        user_selection = input("What would you like? (1-espresso/2-latte/3-cappuccino):\n").lower()
         if user_selection == "off":
             print("Machine turning off...")
-            return
+            break
         elif user_selection == "report":
             report()
-            continue
-        elif user_selection == "1":
-            user_selection = "espresso"
-            # More code
-        elif user_selection == "2":
-            user_selection = "latte"
-            # More code
-        elif user_selection == "3":
-            user_selection = "cappuccino"
-            # More code  
+        elif user_selection in drink_options:
+            drink_name = drink_options[user_selection]
+            prepare_drink(selected_beverage=drink_name)
         else:
             print("Input error...Try again")
-            continue
-
-
-# TODO-4: When the user chooses a drink, the program should check if there are enough
-# resources to make that drink.
 
 
 # TODO-5: If there are sufficient resources to make the drink selected, then the program should
