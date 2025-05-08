4c6e4be1ebaf912a0b75d69ae1a461de8d8ca522
Jay Strong <jay.allen.strong@gmail.com>
2025-05-08 15:24:47 -0400

    feat: implement movement functions for turtle controls in Etch-A-Sketch

--- Day 16-100/Day 19/Etch-A-Sketch/main.py
+++ Day 16-100/Day 19/Etch-A-Sketch/main.py
@@ -4,17 +4,33 @@ tim = Turtle()
 screen = Screen()
 
 def forward():
-    tim.forward()
+    tim.forward(10)
 
 
 def backward():
-    tim.backward()
+    tim.backward(10)
 
+
+def left():
+    tim.left(10)
+
+
+def right():
+    tim.right(10)
+
+
+def clear():
+    tim.reset()
+ 
 
 
 screen.listen()
 screen.onkey(fun=forward, key="w")
 screen.onkey(fun=backward, key="s")
+screen.onkey(fun=left, key="a")
+screen.onkey(fun=right, key="d")
+screen.onkey(fun=clear, key="c")
+
 
 
 
