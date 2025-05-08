bd0dc0d38ff04f88f6b2051d84c5061b6d00d3c9
Jay Strong <jay.allen.strong@gmail.com>
2025-05-08 18:47:01 -0400

    feat: add user betting input and turtle positioning in Turtle Race

--- Day 16-100/Day 19/Turtle Race/main.py
+++ Day 16-100/Day 19/Turtle Race/main.py
@@ -2,11 +2,15 @@ from turtle import Turtle, Screen
 
 red = Turtle()
 screen = Screen()
+screen.setup(width=500, height=400)
 
 red.color("red")
 red.shape("turtle")
-
-
+user_bet = screen.textinput(title="Make Your Bet!", prompt="Which turtle will win the race? Enter a color: ")
+red.penup()
+red.goto(x=-230, y=-100)
+# red.teleport(x=-230, y=-100)
+print(user_bet)
 screen.exitonclick()
 
 # py main.py
