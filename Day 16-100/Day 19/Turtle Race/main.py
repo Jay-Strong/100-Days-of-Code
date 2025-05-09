fcac76ad7b133c203beff91939fb7a719fa56174
Jay Strong <jay.allen.strong@gmail.com>
2025-05-09 05:32:25 -0400

    feat: rearrange user betting input and turtle initialization in Turtle Race

--- Day 16-100/Day 19/Turtle Race/main.py
+++ Day 16-100/Day 19/Turtle Race/main.py
@@ -1,15 +1,16 @@
 from turtle import Turtle, Screen
 
-red = Turtle()
+
 screen = Screen()
 screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make Your Bet!", prompt="Which turtle will win the race? Enter a color: ")
 
+red = Turtle()
 red.color("red")
 red.shape("turtle")
-user_bet = screen.textinput(title="Make Your Bet!", prompt="Which turtle will win the race? Enter a color: ")
+
 red.penup()
 red.goto(x=-230, y=-100)
-# red.teleport(x=-230, y=-100)
 print(user_bet)
 screen.exitonclick()
 
