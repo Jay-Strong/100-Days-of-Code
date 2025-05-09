573e4c7b1604624e1a371dd38c1d035b5c7682fb
Jay Strong <jay.allen.strong@gmail.com>
2025-05-09 16:59:43 -0400

    refactor: streamline movement methods in Snake class for clarity

--- Day 16-100/Day 20 & 21/snake_game/snake.py
+++ Day 16-100/Day 20 & 21/snake_game/snake.py
@@ -3,6 +3,7 @@ import turtle
 START_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 
+
 class Snake:
     def __init__(self) -> None:
         self.segments = []
@@ -15,18 +16,28 @@ class Snake:
             self.segments.append(new_segment)
 
 
-    def turn_left(self) -> None:
-        self.segments[0].left(90)
+    def right(self) -> None:
+        self.segments[0].seth(0)
 
 
-    def turn_right(self) -> None:
-        self.segments[0].right(90)
+    def up(self) -> None:
+        self.segments[0].seth(90)
+
+
+    def left(self) -> None:
+        self.segments[0].seth(180)
+
+
+    def down(self) -> None:
+        self.segments[0].seth(270)
 
 
 
     def move_snake(self) -> None:
-        turtle.onkey(fun=self.turn_left, key="Left")
-        turtle.onkey(fun=self.turn_right, key="Right")
+        turtle.onkey(fun=self.right, key="Right")
+        turtle.onkey(fun=self.up, key="Up")
+        turtle.onkey(fun=self.left, key="Left")
+        turtle.onkey(fun=self.down, key="Down")
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
