0ed1b6ac363f6205129212011c6549d4d7906dba
Jay Strong <jay.allen.strong@gmail.com>
2025-05-09 12:20:36 -0400

    feat: add turtle initialization logic for Snake Game

--- Day 16-100/Day 20 & 21/snake_game/main.py
+++ Day 16-100/Day 20 & 21/snake_game/main.py
@@ -4,6 +4,16 @@ screen = Screen()
 screen.setup(width=600, height= 600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+all_turtles = []
+
+
+for num in range(3):
+    new_turtle = Turtle(shape="square")
+    new_turtle.color("white")
+    new_turtle.penup()
+    new_turtle.goto((-20 * num), 0)
+    all_turtles.append(new_turtle)
+
 
 screen.exitonclick()
 
