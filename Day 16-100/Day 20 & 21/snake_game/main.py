a108c9cf3b4ac9b2583e04bfb6685486268ee869
Jay Strong <jay.allen.strong@gmail.com>
2025-05-09 14:33:25 -0400

    feat: implement Snake class and refactor main game logic for improved structure

--- Day 16-100/Day 20 & 21/snake_game/main.py
+++ Day 16-100/Day 20 & 21/snake_game/main.py
@@ -1,51 +1,15 @@
-from turtle import Screen, Turtle
+from snake import Snake
+from turtle import Screen
 import time
-
 
 screen = Screen()
 screen.setup(width=600, height= 600)
 screen.tracer(0)
 screen.bgcolor("black")
 screen.title("Snake Game")
-segments = []
+screen.listen()
 
-
-for num in range(3):
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto((-20 * num), 0)
-    segments.append(new_segment)
-
-
-def turn_left() -> None:
-    segments[0].left(90)
-
-
-def turn_right() -> None:
-    segments[0].right(90)
-
-
-
-def move_snake() -> None:
-    screen.listen()
-    screen.onkey(fun=turn_left, key="Left")
-    screen.onkey(fun=turn_right, key="Right")
-    
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-    
-    
-    
-    # segments[2].goto(segments[1].position())
-    # segments[1].goto(segments[0].position())
-    # segments[0].forward(20)
-
-
-
+snake = Snake()
 
 screen.update()
 
@@ -53,8 +17,8 @@ game_is_on = True
 
 while game_is_on:
     screen.update()
-    time.sleep(0.2)
-    move_snake()
+    time.sleep(0.1)
+    snake.move_snake()
    
     
 
---
+++ Day 16-100/Day 20 & 21/snake_game/snake.py
@@ -0,0 +1,31 @@
+import turtle
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+
+        for num in range(3):
+            new_segment = turtle.Turtle(shape="square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto((-20 * num), 0)
+            self.segments.append(new_segment)
+
+
+    def turn_left(self) -> None:
+        self.segments[0].left(90)
+
+
+    def turn_right(self) -> None:
+        self.segments[0].right(90)
+
+
+
+    def move_snake(self) -> None:
+        turtle.onkey(fun=self.turn_left, key="Left")
+        turtle.onkey(fun=self.turn_right, key="Right")
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(20)
