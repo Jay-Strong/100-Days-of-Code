7cb23a7f57bca718161948f3f8370ea8f23613d9
Jay Strong <jay.allen.strong@gmail.com>
2025-05-09 09:00:55 -0400

    feat: refactor turtle initialization to use a loop for multiple turtles in Turtle Race

--- Day 16-100/Day 19/Turtle Race/main.py
+++ Day 16-100/Day 19/Turtle Race/main.py
@@ -4,14 +4,16 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make Your Bet!", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-125, -75, -25, 25, 75, 125]
 
-red = Turtle()
-red.color("red")
-red.shape("turtle")
 
-red.penup()
-red.goto(x=-230, y=-100)
-print(user_bet)
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.color(colors[turtle_index])
+    tim.penup()
+    tim.goto(x=-230, y=y_positions[turtle_index])
+
 screen.exitonclick()
 
 # py main.py
