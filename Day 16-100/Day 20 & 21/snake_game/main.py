26945e800ca5b865de436b41f6430d0406c08c7b
Jay Strong <jay.allen.strong@gmail.com>
2025-05-15 09:37:37 -0400

    fix: refine snake collision detection logic for improved accuracy

--- Day 16-100/Day 20 & 21/snake_game/main.py
+++ Day 16-100/Day 20 & 21/snake_game/main.py
@@ -20,14 +20,13 @@ screen.update()
 game_is_on = True
 
 while game_is_on:
-    for segment in snake.segments:
-        if not snake.segments[0]:
-            if snake.head.distance(segment) < 15:
-                game_is_on = False
-                scoreboard.game_over()
-    # if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-    #     game_is_on = False
-    #     scoreboard.game_over()
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
     if snake.head.distance(food) < 18:
         food.refresh()
         scoreboard.add_point()
