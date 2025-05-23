689ed53237d46a576216e2b7ea77d3bbb45df5a6
Jay Strong <jay.allen.strong@gmail.com>
2025-05-23 12:22:54 -0400

    refactor: reorganize ball.py by moving serve constants to the top and improving initialization

--- Day 16-100/Day 22/pong/ball.py
+++ Day 16-100/Day 22/pong/ball.py
@@ -1,8 +1,17 @@
 from turtle import Turtle
+from random import randint
+
+SERVE_TO_LEFT = randint(120, 240)
+SERVE_TO_RIGHT = randint(-60, 60)
 
 class Ball(Turtle):
     def __init__(self) -> None:
         super().__init__()
+        # self.penup()
         self.color("white")
-        self.shape("square")
-        self.seth(90)
+        self.shapesize(stretch_len=5, stretch_wid=5)
+        # self.shape("square")
+        # self.seth(SERVE_TO_LEFT)
+        self.seth(SERVE_TO_RIGHT)
+        self.speed(1)
+        self.forward(600)
